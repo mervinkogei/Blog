7474f1126a4022eb296169a1c7ffdf550de6b58a
boydndonga <boydbugle@gmal.com>
2019-11-14 14:46:34 +0300

    [fixes #169749334 #169749266] create routing for all posts and new post

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,27 @@
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        get("/", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("allPosts", Post.getAllPosts());
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+            Post newPost = new Post();
+            model.put("post", newPost);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
 }
